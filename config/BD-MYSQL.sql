ba07933c837ad4935f523318a7f298130fe4f5de
rodolfogomes <rodolfoaguiargomes@gmail.com>
2018-12-26 13:18:33 -0200

    ajuste tela de notícia

--- config/BD-MYSQL.sql
+++ config/BD-MYSQL.sql
@@ -1,14 +1,15 @@
 --Tabela para construção do portal de notícias.
 
 create table noticias(
-    id_noticia int primary key AUTO_INCREMENT ,
-    descricao varchar(100) not null
+id_noticia int primary key AUTO_INCREMENT ,
+titulo varchar(100) not null,
+noticia varchar(500) not null
 
 );
 
 -- Carga da tabela noticias
-insert into noticias(descricao)values('primeira noticia');
-insert into noticias(descricao)values('segunda noticia');
+insert into noticias(titulo,noticia)values('primeira noticia','estudando node');
+insert into noticias(titulo,noticia)values('segunda noticia','fullstack javascrit');
 
 -- Tratar erro de conexão com o mysql.
     --ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'mysql'
