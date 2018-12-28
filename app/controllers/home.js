37ec0ce96a7753859d68b3962db69f5a5c53dbe4
rodolfogomes <rodolfoaguiargomes@gmail.com>
2018-12-28 20:46:22 -0300

    5 ultimas noticias

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,11 @@
 module.exports.index = function(application, req, res){
-	res.render("home/index");
+	const con = application.config.dbConnection();
+	const noticiasDAO = new application.app.models.NoticiasDAO(con);
+
+	noticiasDAO.get5UltimasNoticias((error,result)=>{
+		console.log(result);
+		res.render("home/index",{noticias: result});
+	});
+	
+	
 }
--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -14,6 +14,10 @@ NoticiasDAO.prototype.salvarNoticia = function(noticia, callback){
 	this._connection.query('insert into noticias set ? ', noticia, callback)
 }
 
+NoticiasDAO.prototype.get5UltimasNoticias = function(callback){
+	this._connection.query('select * from noticias order by data_noticia limit 2',callback)
+}
+
 module.exports = ()=>{
 	return NoticiasDAO;
 }
